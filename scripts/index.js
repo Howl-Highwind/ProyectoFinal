2f68aeeefda4c377d7778dba774481cb15b174c2
Julian Esteban Rodriguez <julian-e-rodriguez999@live.com>
2023-06-16 18:02:01 -0300

    js implementation

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,3 @@
-var aElems = document.getElementsByTagName('li2');
-
-for (var i = 0, len = aElems.length; i < len; i++) {
-    aElems[i].onclick = function() {
-        var check = confirm("¡Atencion! estas llendo a la tienda de ejemplo");
-        if (check == true) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    };
-}​
+function warningfuntion() {
+    alert("¡Atencion! estas llendo a la tienda de ejemplo!");
+  }
